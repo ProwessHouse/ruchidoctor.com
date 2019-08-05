a440ca4320d5cd6645adfed6ffa1cd5f1d6fdde0
Nilam Doctor <nilamdoc@gmail.com>
2019-08-05 20:16:50 +0530

    compact

--- app/views/register/newuser.mail.php
+++ app/views/register/newuser.mail.php
@@ -3,11 +3,11 @@ Hi,<br>
 <br>
 We have recieved your request.<br>
 <br>
-Name: <?=$message['name']?><br>
-Email: <?=$message['email']?><br>
-Phone: <?=$message['phone']?><br>
-Subject: <?=$message['subject']?><br>
-Message: <?=$message['message']?><br>
+Name: <?=$compact['message']['name']?><br>
+Email: <?=$compact['message']['email']?><br>
+Phone: <?=$compact['message']['phone']?><br>
+Subject: <?=$compact['message']['subject']?><br>
+Message: <?=$compact['message']['message']?><br>
 <br>
 - Team Ruchi Doctor<br>
 Date and time: <?=gmdate('Y-m-d H:i:s',time())?><br>
