d706c0341e98037af4d737e4584bb04cc1d76967
Nilam Doctor <nilamdoc@gmail.com>
2020-10-19 21:14:37 +0530

    ruchidoctor

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -6,50 +6,25 @@
 	$pagestarttime = $mtime; 
 
 ?>
-<html lang="en">
+<!DOCTYPE HTML>
+<html>
 
 <head>
-  <!-- meta -->
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>Ruchi Doctor - Publisher, Coach, Trainer, Speaker, Marketer, Curator</title>
-  <meta content="" name="keywords">
-  <meta content="" name="description">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes">
+  <meta http-equiv="Content-Security-Policy" content="default-src * data: gap: content: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400&family=Raleway:wght@100;300;400&family=Share&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/framework7/css/framework7.bundle.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/app.css" type="text/css">
+  <script src="/framework7/js/framework7.bundle.min.js"></script>
+  </head>
 
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
-
-  <!-- Bootstrap CSS File -->
-  <link href="/app/webroot/en/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Libraries CSS Files -->
-  <link href="/app/webroot/en/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
-  <link href="/app/webroot/en/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="/app/webroot/en/lib/magnific-popup/magnific-popup.css" rel="stylesheet">
-  <link href="/app/webroot/en/lib/hover/hover.min.css" rel="stylesheet">
-
-  <!-- Main Stylesheet File -->
-  <link href="/app/webroot/css/style.css" rel="stylesheet">
-
-  <!-- Responsive css -->
-  <link href="/app/webroot/en/css/responsive.css" rel="stylesheet">
-
-  <!-- Favicon -->
-  <link rel="shortcut icon" href="/app/webroot/images/favicon.png">
-
-  <!-- =======================================================
-    Theme Name: Folio
-    Theme URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
-    Author: BootstrapMade.com
-    Author URL: https://bootstrapmade.com
-  ======================================================= -->
-</head>
 <body>
-   		<?php echo $this->_render('element', 'header', compact('pagetotaltime'));?>	
-		<div class="container content">
-			<?php echo $this->content(); ?>
-		</div>
+		<div class="page">
+  <div class="page-content">
+  <?php echo $this->content(); ?>
 <?php 
 	$mtime = microtime();
 	$mtime = explode(" ",$mtime);
@@ -57,24 +32,10 @@
 	$pageendtime = $mtime;
 	$pagetotaltime = ($pageendtime - $pagestarttime);
 ?>
-
-		<?php echo $this->_render('element', 'footer', compact('pagetotaltime'));?>	
-
-
-
-  <!-- End section footer -->
-  <!-- JavaScript Libraries -->
-  <script src="/app/webroot/en/lib/jquery/jquery.min.js"></script>
-  <script src="/app/webroot/en/lib/jquery/jquery-migrate.min.js"></script>
-  <script src="/app/webroot/en/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/app/webroot/en/lib/typed/typed.js"></script>
-  <script src="/app/webroot/en/lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="/app/webroot/en/lib/magnific-popup/magnific-popup.min.js"></script>
-  <script src="/app/webroot/en/lib/isotope/isotope.pkgd.min.js"></script>
-
-
-  <!-- Template Main Javascript File -->
-  <script src="/app/webroot/js/main.js"></script>
+   
+   <?php echo $this->_render('element', 'footer', compact('pagetotaltime'));?>	
+   </div>
+  </div>
 </body>
-
+<!-- JS code -->
 </html>
--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -1,1 +1,22 @@
-<h1>This is BlockChainSage.Net</h1>
+
+
+ <div class="block">
+ <h1 class="sz4 margin" style="border-bottom:10px solid black ">Ruchi Doctor</h1>
+ </div>
+ <div class="row block">
+  <div class="col-50">
+   <img src="/img/ruchi.jpg" width="100%">
+  </div>
+   <div class="col-50">
+    <div class="row">
+     <div class="col-100">
+      <h1 class="Share sz4 cs0"  style="margin-top:0px;margin-bottom:-10px" ><strong>Leader Entrepreneur</strong></h1>
+      <p class="szhalf"><span class="bg-color-red text-color-white" style="font-weight:bold;padding:2px">She</span> is 21st century girl, born in 20th century! There have been many a woman, who has been born in too demanding social and family conditions, and Ruchi is no exception. She was born in a lower middle class, deeply traditional Jain Guajarati family, in a world of stuck up MCPs, with wolves prowling all over, in the guides of human forms. Not only had the home that was loaded with thick chaos, commotion, parents at each other's throats. Mercifully, Ruchi was not the one to cow down in such perpetual inimical, threatening, hostile environ. She never took any bullshit dished out to her in various, disgusting cuisines, rather was focused, determined, and hopeful and empowered.</p>
+      <p class="szhalf"><span class="bg-color-red text-color-white" style="font-weight:bold;padding:2px">She</span> was born, a pro-woman alpha woman, cultivating a meaningful life for a liberated woman, to celebrate life, even though it could be intimidating, yet she never failed to invest in herself, to become tough enough, to be of help for all those sisters, who may not have the zeitgeist, needed, to live with dignity in herself, in eco space chosen by her, for her life.</p>
+      <p class="szhalf"><span class="bg-color-red text-color-white" style="font-weight:bold;padding:2px">She</span> is a hands-on, person who is a go getter, and makes thing happen. Indeed, her fearlessness is contagious. She has always believed that success is not the key to happiness; rather it is the happy women who become successful. Following her own paradigm, she is perpetually in bloom, celebrating forever the spring, where she sings like a nightingale, both in defeat and victories. Thus, she defines success in her own way, and achieving it in her exemplary way and imbibing it in her life, indeed, to pour it over all other, who came close to her Midas touch, both men and women. Like Hillary Clinton, she strongly believes that every girl born has the right to achieve her dreams, and bloody well, she should do it. Her courage lies in her innate strength to recognize her fears and bravely combat them. While combating thus, she just does not dream for success, but makes it happen.</p>
+      <p class="szhalf"><span class="bg-color-red text-color-white" style="font-weight:bold;padding:2px">She</span> has made her success happen right from her childhood. Family was in relentless turmoil, where parents were forever wrangling, making the home in to a long-lasting battle ground, in two people, who never knew, why they came together and for what; more so, they could never ponder on the damaging influence their incessant squabbling would have, on the tender psyche of their children. Surprisingly, Ruchi kept her cool, with her determination never, ever wavering! She had promised herself, to overcome all the demanding obstacles, thwarting her effort, for her rightful, blissful, prosperous life.  Early in life, she had learned the truth that no one would give her anything; she would have to get it for herself with relentless, efforts, many times more than any man, in a man’s world.</p>
+      <p class="szhalf"><span class="bg-color-red text-color-white" style="font-weight:bold;padding:2px">She</span> was inspired by the idea of chasing her bucket list dreams. No longer willing to settle for the lives she felt she should be living, she kept deciding what she wanted to!  She always believed that nothing worth fighting for could be achieved without struggle. And she worked real hard, even to the point being cruel to herself, to excel in her studies. It was because of this, she has under her belt, IT Diploma,  MA English, MA Gujarati etc. It is this dedication and Arjun like focus that all the tumultuous upheavals in her personal life could Never discourage her, from, not only perusing her dreams of success, but actually achieving them.</p>
+     </div>
+    </div>
+  </div>
+ </div>
